5dab0a8c5226a766fee5dbf524c4996b5833628b
Rafa de Castro <rafael@micubiculo.com>
2014-03-28 18:06:37 +0100

    World is round round round...

--- src/mars_clojure/core.clj
+++ src/mars_clojure/core.clj
@@ -1,14 +1,20 @@
 (ns mars-clojure.core)
 
-(def inital-rover {:x 0 :y 0 :facing :north} )
+(def initial-rover {:x 0 :y 0 :facing :north} )
+
+(defn- warp-if-needed [position]
+  (case position
+    -1 100
+    101 0
+    position))
 
 (defmulti execute-command (fn [rover _] (:facing rover)))
 
 (defmethod execute-command :north
   [rover command]
   (case command
-    \f (assoc rover :y (inc (:y rover)))
-    \b (assoc rover :y (dec (:y rover)))
+    \f (assoc rover :y (warp-if-needed (inc (:y rover))))
+    \b (assoc rover :y (warp-if-needed (dec (:y rover))))
     \r (assoc rover :facing :east)
     \l (assoc rover :facing :west)
     )
@@ -17,8 +23,8 @@
 (defmethod execute-command :east
   [rover command]
   (case command
-    \f (assoc rover :x (inc (:x rover)))
-    \b (assoc rover :x (dec (:x rover)))
+    \f (assoc rover :x (warp-if-needed (inc (:x rover))))
+    \b (assoc rover :x (warp-if-needed (dec (:x rover))))
     \r (assoc rover :facing :south)
     \l (assoc rover :facing :north)
     )
@@ -27,8 +33,8 @@
 (defmethod execute-command :west
   [rover command]
   (case command
-    \f (assoc rover :x (dec (:x rover)))
-    \b (assoc rover :x (inc (:x rover)))
+    \f (assoc rover :x (warp-if-needed (dec (:x rover))))
+    \b (assoc rover :x (warp-if-needed (inc (:x rover))))
     \r (assoc rover :facing :north)
     \l (assoc rover :facing :south)
     )
@@ -37,8 +43,8 @@
 (defmethod execute-command :south
   [rover command]
   (case command
-    \f (assoc rover :y (dec (:y rover)))
-    \b (assoc rover :y (inc (:y rover)))
+    \f (assoc rover :y (warp-if-needed (dec (:y rover))))
+    \b (assoc rover :y (warp-if-needed (inc (:y rover))))
     \r (assoc rover :facing :west)
     \l (assoc rover :facing :east)
     )
